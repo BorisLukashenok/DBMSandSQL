04c60df9ddf50abcf60824cdae789f751d82591c
BorisLukashenok <jnardis@yandex.ru>
2023-03-27 07:31:26 +0300

    DZ4

--- DZ/DZ4/DZ4.sql
+++ DZ/DZ4/DZ4.sql
@@ -1,5 +1,7 @@
 use lesson_4;
+
 -- ЗАДАЧА 1 Подсчитать общее количество лайков, которые получили пользователи младше 12 лет.
+
 select sum(d.al) as All_like
 from profiles as p
     join media as m on p.user_id = m.user_id
@@ -10,12 +12,16 @@ from profiles as p
         group by media_id
     ) d on m.id = d.media_id
 where TIMESTAMPDIFF(year, p.birthday, now()) < 12;
+
 -- ЗАДАЧА 2 Определить кто больше поставил лайков (всего): мужчины или женщины. 
+
 select p.gender,
     count(p.gender) as All_like
 from profiles as p
     join likes as l on l.user_id = p.user_id
 group by p.gender;
+
+
 select case
         when p.gender = 'f' then 'Женщины'
         else 'Мужчины'
@@ -26,12 +32,16 @@ from profiles as p
 group by p.gender
 order by All_like desc
 limit 1;
+
 -- ЗАДАЧА 3 Вывести всех пользователей, которые не отправляли сообщения.
+
 select CONCAT(u.firstname, ' ', u.lastname) AS 'Пользователь'
 from users as u
     left join messages m on u.id = m.from_user_id
 where m.from_user_id is null;
+
 -- ЗАДАЧА 4 (по желанию)* Пусть задан некоторый пользователь. Из всех друзей этого пользователя найдите человека, который больше всех написал ему сообщений.
+
 select CONCAT(u.firstname, ' ', u.lastname) as fullname, d.Alls
 from (
         select m.from_user_id,
